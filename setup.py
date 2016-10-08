24c1c22d738be1b93955bc9c4457b69780682d83
Philipp Schmitt <philipp@schmitt.co>
2016-10-08 16:42:16 +0200

    Bump version to 2.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pykeepass',
-    version='2.0',
+    version='2.0.1',
     license='GPL3',
     description='Low-level library to interact with keepass databases '\
                 '(supports the v.4 format)',
