88f53b1e105e4977fe07bf2b770544b5983ab07a
evan <evan@evanw.org>
2017-06-27 00:27:41 -0400

    add dateutil to requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pykeepass',
-    version='2.7.1',
+    version='2.7.0',
     license='GPL3',
     description='Low-level library to interact with keepass databases '\
                 '(supports the v.4 format)',
@@ -12,7 +12,7 @@ setup(
     author_email='philipp@schmitt.co',
     url='https://github.com/pschmitt/pykeepass',
     packages=find_packages(),
-    install_requires=['libkeepass', 'easypysmb', 'dateutil'],
+    install_requires=['libkeepass', 'easypysmb', 'python-dateutil'],
     entry_points={
         'console_scripts': ['pkpwrite=pykeepass.pkpwrite:main']
     }
