9b0e103efa20588f65492b4544a77b05f6a190ef
Philipp Schmitt <philipp@schmitt.co>
2018-09-07 10:36:59 +0200

    Use double quotes

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='pykeepass',
-    version='3.0.2',
-    license='GPL3',
-    description='Python library to interact with keepass databases '
-                '(supports KDBX3 and KDBX4)',
-    long_description=open('README.rst').read(),
-    author='Philipp Schmitt',
-    author_email='philipp@schmitt.co',
-    url='https://github.com/pschmitt/pykeepass',
+    name="pykeepass",
+    version="3.0.2",
+    license="GPL3",
+    description="Python library to interact with keepass databases "
+                "(supports KDBX3 and KDBX4)",
+    long_description=open("README.rst").read(),
+    author="Philipp Schmitt",
+    author_email="philipp@schmitt.co",
+    url="https://github.com/pschmitt/pykeepass",
     packages=find_packages(),
     install_requires=[
-        'python-dateutil',
+        "python-dateutil",
         "construct",
         "argon2_cffi",
         "pycryptodome",
