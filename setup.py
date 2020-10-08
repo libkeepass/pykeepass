12c38fa9bb529af8b4a7a3d333c50acd9556ca22
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2020-10-09 00:48:51 +0200

    move version.__version__ to pykeepass.__version__

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import find_packages, setup
-from pykeepass.version import __version__
+from pykeepass import __version__
 
 with open("README.rst") as file:
     README = file.read()
