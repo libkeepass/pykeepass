libkeepass/pykeepass

commit f0ca951d73136c92c161ca655d7b428c8ad04a3f

Move package constants from setup() into package itself (#203)
* open README with context manager

* create version.py for __version__ import

* move version.__version__ to pykeepass.__version__

pykeepass/__init__.py
@@ -1,2 +1,4 @@
 from __future__ import absolute_import
 from .pykeepass import PyKeePass, create_database
+
+__version__ = "3.2.1"

setup.py
@@ -1,13 +1,16 @@
 from setuptools import find_packages, setup
+from pykeepass import __version__
 
+with open("README.rst") as file:
+    README = file.read()
 
 setup(
     name="pykeepass",
-    version="3.2.1",
+    version=__version__,
     license="GPL3",
     description="Python library to interact with keepass databases "
                 "(supports KDBX3 and KDBX4)",
-    long_description=open("README.rst").read(),
+    long_description=README,
     author="Philipp Schmitt",
     author_email="philipp@schmitt.co",
     url="https://github.com/pschmitt/pykeepass",

commit 63fec32d28f927ef04cad41f8682d09bbcbeb562

Fix install issue introduced by #203

pykeepass/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import
 from .pykeepass import PyKeePass, create_database
 
-__version__ = "3.2.1"
+from pykeepass.version import __version__

pykeepass/version.py
@@ -1,0 +1,1 @@
+__version__ = "3.2.1"

setup.py
@@ -9,7 +9,7 @@ setup(
     version=__version__,
     license="GPL3",
     description="Python library to interact with keepass databases "
-                "(supports KDBX3 and KDBX4)",
+    "(supports KDBX3 and KDBX4)",
     long_description=README,
     author="Philipp Schmitt",
     author_email="philipp@schmitt.co",
@@ -23,7 +23,7 @@ setup(
         "pycryptodomex>=3.6.2",
         "lxml",
         # FIXME python2
-        "future"
+        "future",
     ],
-    include_package_data=True
+    include_package_data=True,
 )