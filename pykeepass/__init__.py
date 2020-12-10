8311f1b6a8936b7b4cdd817bcd57d3aad8c4a9b1
Philipp Schmitt <philipp@schmitt.co>
2020-12-10 08:16:24 +0100

    Fix F401: imported but unused

--- pykeepass/__init__.py
+++ pykeepass/__init__.py
@@ -3,3 +3,5 @@ from __future__ import absolute_import
 from pykeepass.version import __version__
 
 from .pykeepass import PyKeePass, create_database
+
+__all__ = ["__version__", PyKeePass, create_database]
