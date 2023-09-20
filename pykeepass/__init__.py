71c0d1157448594da5c1cebcacaa49db57df1af2
evan <evan@evanw.org>
2023-09-20 10:58:19 -0500

    add exports to __init__.py

--- pykeepass/__init__.py
+++ pykeepass/__init__.py
@@ -2,3 +2,5 @@ from __future__ import absolute_import
 from .pykeepass import PyKeePass, create_database
 
 from .version import __version__
+
+__all__ = ["PyKeePass", "create_database", "__version__"]
