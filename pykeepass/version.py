7611eadee6918345b82109c255b65e748711a7bd
evan <evan@evanw.org>
2024-02-09 23:46:22 -0600

    backwards compatible __version__

--- pykeepass/version.py
+++ pykeepass/version.py
@@ -1,4 +1,5 @@
-import importlib.metadata
-
-__version__ = importlib.metadata.version('pykeepass')
 __all__= ["__version__"]
+
+# FIXME: switch to using importlib.metadata when dropping Python<=3.7
+import pkg_resources
+__version__ = pkg_resources.get_distribution('pykeepass').version
