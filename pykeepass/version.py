3794220a94137183c97d53d2c99371bd60d13b4c
Sebastian Spaeth <Sebastian@SSpaeth.de>
2026-03-09 14:54:23 +0100

    Drop pkg_resources module, since that is only python <3.8
    Since pykeepass dropped support for python <3.8, we can remove the
    hack to rely on setuptools' pkg_resources for these versions.
    
    Also, we need to record the dependency on importlib-metadata, since
    this is otherwise not installed and attempted to fall back on the
    obsolete pkg_resources codepath.

--- pykeepass/version.py
+++ pykeepass/version.py
@@ -1,10 +1,4 @@
 __all__= ["__version__"]
 
-try:
-    # Retrieval of metadata version for Python 3.8 and above
-    from importlib.metadata import version
-    __version__ = version('pykeepass')
-except ImportError:
-    # Fallback for older Python versions (< 3.8)
-    from pkg_resources import get_distribution
-    __version__ = get_distribution('pykeepass').version
+from importlib.metadata import version
+__version__ = version('pykeepass')
